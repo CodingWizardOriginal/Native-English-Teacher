f71d4c4090a44ccecc62fd9220a96e35346bb1bc
dubryenglish <48644903+dubryenglish@users.noreply.github.com>
2019-03-19 13:39:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var person= {
+name: 'Santiago',
+age: 17,
+job: 'Teacher',
+}
+
+console.log(name)
+
+object.keys(person) varObject 
+console.log(personKeys);['name', 'job', 'city']
+
+var personValues = 
+
+
+
+
